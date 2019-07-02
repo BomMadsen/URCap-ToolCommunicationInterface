5f8ec5d9d7542078e498fc8b001a2edffe339489
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2019-07-02 11:52:37 +0200

    Created ToolIOController class

---
+++ com.jbm.urcap.toolcommunicationinterface/src/main/java/com/jbm/urcap/toolcommunicationinterface/tci/ToolIOController.java
@@ -0,0 +1,21 @@
+package com.jbm.urcap.toolcommunicationinterface.tci;
+
+import com.ur.urcap.api.domain.resource.tooliointerface.control.ToolIOInterfaceControlEvent;
+import com.ur.urcap.api.domain.resource.tooliointerface.control.ToolIOInterfaceController;
+
+public class ToolIOController implements ToolIOInterfaceController{
+	
+
+	@Override
+	public void onControlGranted(ToolIOInterfaceControlEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onControlToBeRevoked(ToolIOInterfaceControlEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
