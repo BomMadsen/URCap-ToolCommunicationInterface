158c453b0f23f283f98d478206a88da6a527b8ff
jacobbsmadsen@gmail.com <jacobbsmadsen@gmail.com>
2019-07-02 11:57:42 +0200

    Added construction to ToolIOController

--- com.jbm.urcap.toolcommunicationinterface/src/main/java/com/jbm/urcap/toolcommunicationinterface/tci/ToolIOController.java
+++ com.jbm.urcap.toolcommunicationinterface/src/main/java/com/jbm/urcap/toolcommunicationinterface/tci/ToolIOController.java
@@ -1,20 +1,30 @@
 package com.jbm.urcap.toolcommunicationinterface.tci;
 
+import com.ur.urcap.api.domain.resource.ResourceModel;
+import com.ur.urcap.api.domain.resource.tooliointerface.ToolIOInterface;
 import com.ur.urcap.api.domain.resource.tooliointerface.control.ToolIOInterfaceControlEvent;
+import com.ur.urcap.api.domain.resource.tooliointerface.control.ToolIOInterfaceControllable;
 import com.ur.urcap.api.domain.resource.tooliointerface.control.ToolIOInterfaceController;
+import com.ur.urcap.api.domain.system.capability.CapabilityManager;
 
 public class ToolIOController implements ToolIOInterfaceController{
 	
+	private final CapabilityManager capabilityManager;					// For validating, that the right capabilities (TCI) are supported
+	private final ToolIOInterface tooIOInterface;						// The read-only instance of the Tool IO Interface
+	private ToolIOInterfaceControllable toolIOInterfaceControllable;	// The controllable instance of the Tool IO Interface
+	
+	public ToolIOController(ResourceModel resourceModel, CapabilityManager capabilityManager) {
+		this.capabilityManager = capabilityManager;
+		this.tooIOInterface = resourceModel.getToolIOInterface();
+	}
 
 	@Override
 	public void onControlGranted(ToolIOInterfaceControlEvent event) {
-		// TODO Auto-generated method stub
-		
+		toolIOInterfaceControllable = event.getControllableResource();
 	}
 
 	@Override
 	public void onControlToBeRevoked(ToolIOInterfaceControlEvent event) {
-		// TODO Auto-generated method stub
 		
 	}
 
